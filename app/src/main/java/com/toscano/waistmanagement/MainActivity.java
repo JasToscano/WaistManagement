a4aa7f030b048ece97d04e05a7d5d657a0374db4
JasToscano <jasminetoscano@gmail.com>
2023-10-15 17:14:46 -0700

    3rd Commit

--- app/src/main/java/com/toscano/waistmanagement/MainActivity.java
+++ app/src/main/java/com/toscano/waistmanagement/MainActivity.java
@@ -2,41 +2,134 @@ package com.toscano.waistmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private void loginSuccess(View view){
+    EditText username;
+    EditText password;
+    Button loginBTN;
+    TextView passwordMSG;
+    UserData _db;
+    UserModel validUser;
+    WeightData _weights;
+    private DialogInterface.OnClickListener dialogClickListener;
+
+    private void loginSuccess(View view, String _user){
+
+        //start the weight Database
+        _weights = WeightData.getInstance(this);
+
+        //instantiate UserModel
+        validUser = UserModel.getUserInstance();
+        validUser.setUserName(_user);
+        validUser.setGoal(_weights.getGoal(validUser));
+
         Intent intent = new Intent(this, MainScreen.class);
         startActivity(intent);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText username =(EditText) findViewById(R.id.textUsername);
-        EditText password =(EditText) findViewById(R.id.textPassword);
+        username = findViewById(R.id.textUsername);
+        password = findViewById(R.id.textPassword);
+        loginBTN = findViewById(R.id.buttonLogin);
+        passwordMSG = findViewById(R.id.passwordMessage);
 
-        Button loginbtn = (Button) findViewById(R.id.buttonLogin);
+        //call singleton
+        _db = UserData.getInstance(this);
 
-        //user and user
-        loginbtn.setOnClickListener(new View.OnClickListener() {
+        //admin and admin
+        loginBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(username.getText().toString().equals("user") && password.getText().toString().equals("user")){
-                    //correct
-                    loginSuccess(v);
-                }else
+
+                String _username = username.getText().toString();
+                String _password = password.getText().toString();
+
+                boolean validUser = _db.checkUserName(_username);
+                boolean validPass = _db.checkUserPassword(_username, _password);
+
+                //first test if a registered user
+                if(validUser){
+                    if(validPass){
+                        Toast.makeText(MainActivity.this,"Login Success",Toast.LENGTH_SHORT).show();
+                        loginSuccess(v, _username);
+                    }
+                    else{
+                        Toast.makeText(MainActivity.this,"Incorrect Password!!!",Toast.LENGTH_SHORT).show();
+                    }
+                }
+                //testing
+                else if(_username.equals("admin") && _password.equals("admin")){
+
+                    loginSuccess(v, "admin");
+                }
+                //to do: auto register a user if the username isn't valid
+                //From http://www.wolinlabs.com/blog/yes.no.message.box.html
+                else if(!validUser) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder
+                            .setTitle("Account Not Recognized")
+                            .setMessage("Would you like to register?")
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                    //call insertUser
+                                    Boolean userAdded = _db.insertUser(_username, _password);
+
+                                    if(userAdded){
+                                        Toast.makeText(MainActivity.this,"Account added",Toast.LENGTH_SHORT).show();
+                                    }
+                                    else{
+                                        Toast.makeText(MainActivity.this,"Account failed",Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            })
+                            .setNegativeButton("No", null) //Do nothing on no
+                            .show();
+                }
+                else {
                     //incorrect
-                    Toast.makeText(MainActivity.this,"LOGIN FAILED.",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "LOGIN FAILED!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            //if the passwords meets the minimum
+            //enable the button
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length() < 5) {
+                    passwordMSG.setVisibility(View.VISIBLE);
+                } else {
+                    passwordMSG.setVisibility(View.INVISIBLE);
+                    loginBTN.setEnabled(true);
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
     }
+
 }
--- app/src/main/java/com/toscano/waistmanagement/WeightEnter.java
+++ app/src/main/java/com/toscano/waistmanagement/WeightEnter.java
@@ -8,13 +8,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.Calendar;
 
 
 public class WeightEnter extends AppCompatActivity {
 
     protected EditText dateEntry;
+    protected EditText weightEntry;
+    protected String isoDate;
+    UserModel _user;
+    WeightData _db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,34 +26,40 @@ public class WeightEnter extends AppCompatActivity {
         setContentView(R.layout.activity_weight_enter);
 
         dateEntry = findViewById(R.id.editWeightDate);
+        weightEntry = findViewById(R.id.editWeightWeight);
+
+        _user = UserModel.getUserInstance();
+        _db = WeightData.getInstance(this);
 
         dateEntry.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                //calendar instance.
+
+                // instance of our calendar.
                 final Calendar c = Calendar.getInstance();
 
-                //day, month and year.
+                // our day, month and year.
                 int year = c.get(Calendar.YEAR);
                 int month = c.get(Calendar.MONTH);
                 int day = c.get(Calendar.DAY_OF_MONTH);
 
-                //variable for date picker dialog.
+                // date picker dialog.
                 DatePickerDialog datePickerDialog = new DatePickerDialog(
-                        //passing context.
+                        // passing context.
                         WeightEnter.this,
                         new DatePickerDialog.OnDateSetListener() {
                             @Override
                             public void onDateSet(DatePicker view, int year,
                                                   int monthOfYear, int dayOfMonth) {
-                                //set date to our edit text.
+                                // set date to our edit text.
                                 dateEntry.setText((monthOfYear + 1) + "-" + dayOfMonth  + "-" + year);
-
+                                isoDate = String.valueOf(year) + "-" + String.valueOf(monthOfYear + 1) + "-" +
+                                        String.valueOf(dayOfMonth);
                             }
                         },
-                        //passing year, month and day for selected date.
+
                         year, month, day);
-                //display date picker dialog.
+
                 datePickerDialog.show();
             }
 
@@ -59,5 +69,29 @@ public class WeightEnter extends AppCompatActivity {
     public void openMainForm(View view){
         Intent intent = new Intent(this, MainScreen.class);
         startActivity(intent);
+    }
+    public void onSaveClick(View view){
+        String date = dateEntry.getText().toString();
+        String sWeight = weightEntry.getText().toString();
+        float weight = 0;
+
+        //protect against an empty form
+        if (!date.equals("") && !sWeight.equals("")){
+            weight = Float.valueOf(sWeight);
+            WeightClass entry = new WeightClass(isoDate, weight);
+            Boolean weightAdd = _db.addEntry(entry, _user);
+
+            //If goal reached and SMS on then send the message
+            if(weight <= _user.getGoal()){
+                if(_user.isTextPermission()){
+                    SMSMessage.sendLongSMS(_user.getSMSText());
+                }
+            }
+        }
+
+        //back to main
+        Intent intent = new Intent(this, MainScreen.class);
+        startActivity(intent);
+
     }
 }
